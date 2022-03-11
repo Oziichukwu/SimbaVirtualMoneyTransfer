76bf264933f70eb90432becd3d9125c15f45b56d
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-03-11 03:51:03 +0100

    exception added

--- src/main/java/com/example/simbavirtualmoneytransfer/web/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/simbavirtualmoneytransfer/web/exceptions/UserNotFoundException.java
@@ -1,2 +1,19 @@
-package com.example.simbavirtualmoneytransfer.web.exceptions;public class UserNotFoundException {
+package com.example.simbavirtualmoneytransfer.web.exceptions;
+
+public class UserNotFoundException extends SimbaVirtaulMoneyException {
+
+    public UserNotFoundException() {
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
 }
