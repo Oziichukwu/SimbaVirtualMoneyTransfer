b79078ba6dae1aa207f2b2f813c9c20fc52bf61b
Oziichukwu <ukaegbugoodnews13@gmail.com>
2022-03-11 04:17:25 +0100

    account repository updated

--- src/main/java/com/example/simbavirtualmoneytransfer/dtos/response/TransactionResponse.java
+++ src/main/java/com/example/simbavirtualmoneytransfer/dtos/response/TransactionResponse.java
@@ -1,10 +1,12 @@
 package com.example.simbavirtualmoneytransfer.dtos.response;
 
 import com.example.simbavirtualmoneytransfer.data.models.Currency;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TransactionResponse {
 
    private boolean isSuccess;
